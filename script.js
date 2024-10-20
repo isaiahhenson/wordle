93b42e48449aabf38d837c5b0560aca4aa798eb2
Isaiah Henson <Devpuddles@gmail.com>
2024-10-20 16:08:12 -0400

    added logic to project

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ function initBoard() {
 }
 
 initBoard();
+
+document.addEventListener('keyup', (e) => {
+  if (guessesRemaining === 0) {
+    return;
+  }
+
+  let pressedKey = String(e.key);
+  if (pressedKey === 'Backspace' && nextLetter !== 0) {
+    deleteLetter();
+    return;
+  }
+
+  if (pressedKey === 'Enter') {
+    checkGuess();
+    return;
+  }
+
+  let found = pressKey.match(/[a-z]/gi);
+
+  if (!found || found.length > 1) {
+    return;
+  } else {
+    insertLetter(pressedKey);
+  }
+});
+
+function insertLetter(pressedKey) {
+  if (nextLetter === 5) {
+    return;
+  }
+  pressedKey = pressedKey.toLowerCase();
+
+  let row = document.getElementsByClassName('letter-row')[6 - guessesRemaining];
+  let box = row.children[nextLetter];
+  box.textContent = pressedKey;
+  box.classList.add('filled-box');
+  currentGuess.push(pressedKey);
+  nextLetter += 1;
+}
